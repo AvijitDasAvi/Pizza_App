419113856919130e5f87478966f2f5db2b51feca
Avijit Das <avijitavi338895@gmail.com>
2024-08-19 00:07:08 +0600

    Complete splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pizza_app/screens/home_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  get splash => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Center(
+        child: Lottie.asset("assets/Lottie/Animation - 1723998453080.json"),
+      ),
+      nextScreen: const HomeScreen(),
+      duration: 3500,
+      splashIconSize: 400,
+      backgroundColor: const Color.fromARGB(255, 224, 207, 48),
+    );
+  }
+}
